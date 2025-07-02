893f3ba3855334265a4a6c08c0d4a3b02200388a
kunal2004-dotcom <kunalbagad2004@gmail.com>
2025-07-02 14:08:25 +0530

    code updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,4 +56,5 @@ if submit_button and user_input.strip():
             st.error(" Bot failed to respond: " + response.text)
 
     except Exception as e:
-        raise f"The response failed due to {e}"
+        raise Exception(f"The response failed due to {e}")
+    
